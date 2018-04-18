4258e9c571b65de711889d0adce7cbed68731e2d
Julie K <julieskim160@gmail.com>
2018-04-18 13:53:51 -0700

    async promises

--- src/components/complaint-form.js
+++ src/components/complaint-form.js
@@ -1,22 +1,55 @@
 import React from 'react';
-import {reduxForm, Field} from 'redux-form';
+import {reduxForm, Field, SubmissionError} from 'redux-form';
+//focus
 import {required, nonEmpty, fiveCharacters, nonNumber} from '../validator';
 import './complaint-form.css';
 
 class ComplaintForm extends React.Component {
     onSubmit(values) {
-
-    }
+        console.log(values);
+        return fetch('https://us-central1-delivery-form-api.cloudfunctions.net/api/report/', {
+            method: 'POST',
+            body: JSON.stringify(values),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(res => {
+                if (!res.ok) {
+                    if (
+                        res.headers.has('content-type') && res.headers
+                            .get('content-type')
+                            .startsWith('application/json')
+                    ){
+                        return res.json().then(err => Promise.reject(err));
+                    }
+                    return Promise.reject({
+                        code: res.status,
+                        message: res.statusText
+                    })
+                }
+                return;
+            })
+            .then(() => console.log('Submitted with values', values))
+            .catch(error => {
+                return Promise.reject(
+                    new SubmissionError({
+                        [error.location]: error.message
+                    })
+                )
+            });
+    };
     render() {
-        if(this.props.submitSucceeded) {
-            return <div> We will be with you shortly </div>;
-        }
+        // if(this.props.submitSucceeded) {
+        //     return <div> Thank you for your input </div>;
+        // }
         // console.log(this.props)
         return (
             <form 
-              onSubmit={this.props.handleSubmit(values=> 
-                this.onSubmit(values))
-              }>
+              onSubmit={this.props.handleSubmit(values=> {
+                  console.log(values);
+                this.onSubmit(values) }
+            )}>
                 <label htmlFor="trackingNumber">Tracking Number</label> <br/>
                 <Field 
                     component="input" 
@@ -25,16 +58,16 @@ class ComplaintForm extends React.Component {
                     id="trackingNumber"
                     validate={[required, nonEmpty, fiveCharacters, nonNumber]}
                 /> <br/>
-                <label htmlFor="details">What is your issue</label> <br/>
-                <Field component="select" name="selectOptions"> 
-                    <option value='notArrived' >My delivery hasn't arrived</option>
-                    <option value='wrongItem' >The wrong item was delivered</option>
-                    <option value='missing' >Part of my order was missing</option>
+                <label htmlFor="issue">What is your issue</label> <br/>
+                <Field component="select" name="issue"> 
+                    <option value='not-delivered' >My delivery hasn't arrived</option>
+                    <option value='wrong-item' >The wrong item was delivered</option>
+                    <option value='missing-part' >Part of my order was missing</option>
                     <option value='damaged' >Some of my order arrived damaged</option>
                     <option value='other' >Other(give details below)</option>
                 </Field> <br/>
                 <label htmlFor="details">Give more details(optional)</label> <br/>
-                <Field component="textarea" name="textarea"> 
+                <Field component="textarea" name="details"> 
                     rows="3" cols="50" name="details" id="details"> 
                 </Field> <br/>
                 <button type="submit">Submit</button>
@@ -45,5 +78,5 @@ class ComplaintForm extends React.Component {
 
 
 export default reduxForm({
-    form: 'register'
+    form: 'complaint'
 })(ComplaintForm)
