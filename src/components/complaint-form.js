ba598b53b31742d3c6397c7b6babcd0de1d311d3
Julie K <julieskim160@gmail.com>
2018-04-18 10:47:45 -0700

    Add field to select options

--- src/components/complaint-form.js
+++ src/components/complaint-form.js
@@ -4,6 +4,9 @@ import './complaint-form.css';
 
 class ComplaintForm extends React.Component {
     render() {
+        if(this.props.submitSucceeded) {
+            return <div> Thanks for registering </div>;
+        }
         return (
             <form 
               onSubmit={this.props.handleSubmit(values=> 
@@ -12,15 +15,15 @@ class ComplaintForm extends React.Component {
                 <label htmlFor="trackingNumber">Tracking Number</label> <br/>
                 <Field component="input" type="text" name="trackingNumber" id="trackingNumber" /> <br/>
                 <label htmlFor="details">What is your issue</label> <br/>
-                <select> 
+                <Field component="select" name="selectOptions"> 
                     <option value='notArrived' >My delivery hasn't arrived</option>
                     <option value='wrongItem' >The wrong item was delivered</option>
                     <option value='missing' >Part of my order was missing</option>
                     <option value='damaged' >Some of my order arrived damaged</option>
                     <option value='other' >Other(give details below)</option>
-                </select> <br/>
+                </Field> <br/>
                 <label htmlFor="details">Give more details(optional)</label> <br/>
-                <textarea> rows="3" cols="50" name="details" id="details"> </textarea> <br/>
+                <Field component="textarea" name="textarea"> rows="3" cols="50" name="details" id="details"> </Field> <br/>
                 <button type="submit">Submit</button>
             </form>
         )
