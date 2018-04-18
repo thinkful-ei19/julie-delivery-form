20c95b1931d86acab1b2f2e52ccceb914b2a9de7
Julie K <julieskim160@gmail.com>
2018-04-18 09:46:42 -0700

    use field form in complaint form

--- src/components/complaint-form.js
+++ src/components/complaint-form.js
@@ -1,22 +1,26 @@
 import React from 'react';
-import {reduxForm} from 'redux-form';
+import {reduxForm, Field} from 'redux-form';
+import './complaint-form.css';
 
 class ComplaintForm extends React.Component {
     render() {
         return (
-            <form>
+            <form 
+              onSubmit={this.props.handleSubmit(values=> 
+                console.log(values))
+              }>
                 <label htmlFor="trackingNumber">Tracking Number</label> <br/>
-                <input type="text" name="trackingNumber" id="trackingNumber" /> <br/>
+                <Field component="input" type="text" name="trackingNumber" id="trackingNumber" /> <br/>
                 <label htmlFor="details">What is your issue</label> <br/>
                 <select> 
-                    <option value='' >My delivery hasn't arrived</option>
-                    <option value='' >The wrong item was delivered</option>
-                    <option value='' >Part of my order was missing</option>
-                    <option value='' >Some of my order arrived damaged</option>
-                    <option value='' >Other(give details below)</option>
+                    <option value='notArrived' >My delivery hasn't arrived</option>
+                    <option value='wrongItem' >The wrong item was delivered</option>
+                    <option value='missing' >Part of my order was missing</option>
+                    <option value='damaged' >Some of my order arrived damaged</option>
+                    <option value='other' >Other(give details below)</option>
                 </select> <br/>
                 <label htmlFor="details">Give more details(optional)</label> <br/>
-                <textarea rows="3" cols="50" name="details" id="details"> </textarea> <br/>
+                <textarea> rows="3" cols="50" name="details" id="details"> </textarea> <br/>
                 <button type="submit">Submit</button>
             </form>
         )
