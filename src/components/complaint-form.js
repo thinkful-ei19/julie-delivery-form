9f68bb75a0e9fdcd6544fbeaeff85f54d207b1dd
Julie K <julieskim160@gmail.com>
2018-04-18 09:33:48 -0700

    complaint form

--- src/components/complaint-form.js
+++ src/components/complaint-form.js
@@ -5,10 +5,19 @@ class ComplaintForm extends React.Component {
     render() {
         return (
             <form>
-                <label htmlFor="trackingNumber">Tracking Number</label>
-                <input type="text" name="trackingNumber" id="trackingNumber" />
-                
-                <label htmlFor="details">Give more details(optional)</label>
+                <label htmlFor="trackingNumber">Tracking Number</label> <br/>
+                <input type="text" name="trackingNumber" id="trackingNumber" /> <br/>
+                <label htmlFor="details">What is your issue</label> <br/>
+                <select> 
+                    <option value='' >My delivery hasn't arrived</option>
+                    <option value='' >The wrong item was delivered</option>
+                    <option value='' >Part of my order was missing</option>
+                    <option value='' >Some of my order arrived damaged</option>
+                    <option value='' >Other(give details below)</option>
+                </select> <br/>
+                <label htmlFor="details">Give more details(optional)</label> <br/>
+                <textarea rows="3" cols="50" name="details" id="details"> </textarea> <br/>
+                <button type="submit">Submit</button>
             </form>
         )
     }
